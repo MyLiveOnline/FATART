5d37db5046a6ecab16f7556d4e0f48902c880736
MyLiveOnline <KecS50@yandex.ru>
2018-09-14 14:33:18 +0500

    test

---
+++ index.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: artemfaktyhov
+ * Date: 27.08.2018
+ * Time: 23:49
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <title>FATART</title>
+    <meta charset="utf-8">
+    <meta name="Keywords" content="Мультфильмы"/>
+    <meta name="Description" content="Серии"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="./src/theme/css/main.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link href="./src/theme/css/MBD/css/mdb.min.css" rel="stylesheet">
+</head>
+<body>
+
+
+<header>
+    <div class="header">
+        <div id="header-sroll" class="header-text">FATART</div>
+    </div>
+</header>
+
+<div class="grid content case">
+    <div class="item">
+        <div class="item__details">
+            jelly-o brownie sweet
+        </div>
+    </div>
+    <div class="item item--large">
+        <div class="item__details">
+            Muffin jelly gingerbread
+        </div>
+    </div>
+    <div class="item item--medium">
+        <div class="item__details">
+            sesame snaps chocolate
+        </div>
+    </div>
+    <div class="item item--large">
+        <div class="item__details">
+            Oat cake
+        </div>
+    </div>
+    <div class="item item--full">
+        <div class="item__details">
+            jujubes cheesecake
+        </div>
+    </div>
+    <div class="item item--medium">
+        <div class="item__details">
+            Dragée pudding brownie
+        </div>
+    </div>
+    <div class="item item--large">
+        <div class="item__details">
+            Oat cake
+        </div>
+    </div>
+    <div class="item">
+        <div class="item__details">
+            powder toffee
+        </div>
+    </div>
+    <div class="item item--medium">
+        <div class="item__details">
+            pudding cheesecake
+        </div>
+    </div>
+    <div class="item item--large">
+        <div class="item__details">
+            toffee bear claw
+        </div>
+    </div>
+    <div class="item">
+        <div class="item__details">
+            cake cookie croissant
+        </div>
+    </div>
+    <div class="item item--medium">
+        <div class="item__details">
+            liquorice sweet roll
+        </div>
+    </div>
+    <div class="item item--medium">
+        <div class="item__details">
+            chocolate marzipan
+        </div>
+    </div>
+    <div class="item item--large">
+        <div class="item__details">
+            danish dessert lollipop
+        </div>
+    </div>
+    <div class="item">
+        <div class="item__details">
+            sugar plum dragée
+        </div>
+    </div>
+</div>
+
+<script src="./src/theme/js/script.js"></script>
+</body>
+</html>
